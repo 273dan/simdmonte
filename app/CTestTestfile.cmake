5838d4fb2ead14243e527eba4f8c55dd0230feaa
273dan <dankirby273@gmail.com>
2025-10-24 00:17:07 +0100

    More gitignore tweaks

--- app/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dan/Dev/simdmonte/app
-# Build directory: /home/dan/Dev/simdmonte/app
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
